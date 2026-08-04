76863f514a3cd67969fea4a44bbe2b5c8b018632
gama-hijau <asnul05@gmail.com>
2026-08-05 02:14:27 +0800

    Zoom out 6 foto Proker Individu (Asnul dipertahankan)
    Asnul sudah dikonfirmasi pas, tidak diubah. 6 foto lainnya di-zoom
    out supaya seluruh kepala sampai leher kelihatan, dengan wajah tetap
    jadi titik tengah crop.

--- docs/sw.js
+++ docs/sw.js
@@ -10,7 +10,7 @@
      lewat localStorage supaya ada label waktu pembaruan.
    ============================================================ */
 
-var VERSI_CACHE = 'gama-hijau-v52';
+var VERSI_CACHE = 'gama-hijau-v53';
 var CACHE_FONT = 'gama-font-v1';
 
 var ASET_APLIKASI = [
