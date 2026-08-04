f277125789faf42bd7890e928dd68d754255e823
gama-hijau <asnul05@gmail.com>
2026-08-04 11:58:18 +0800

    Tambah varian logo UNHAS teks putih khusus untuk header gelap
    Tulisan "UNIVERSITAS HASANUDDIN" pada logo gabungan aslinya abu-abu
    gelap, nyaris tak terlihat di latar hijau tua header. Dibuatkan
    varian logo-unhas-kkn-gelap.png (teks diputihkan) khusus dipakai di
    header; logo-unhas-kkn.png (teks abu-abu asli) tetap dipakai di
    kartu Data Kelurahan yang berlatar terang.

--- docs/sw.js
+++ docs/sw.js
@@ -10,7 +10,7 @@
      lewat localStorage supaya ada label waktu pembaruan.
    ============================================================ */
 
-var VERSI_CACHE = 'gama-hijau-v31';
+var VERSI_CACHE = 'gama-hijau-v32';
 var CACHE_FONT = 'gama-font-v1';
 
 var ASET_APLIKASI = [
