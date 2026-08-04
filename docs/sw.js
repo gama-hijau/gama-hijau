94b528f3094a3e5ff03e79f1053b10559bc6d50f
gama-hijau <asnul05@gmail.com>
2026-08-04 12:24:04 +0800

    Perbaiki ukuran logo Kelurahan Galung Maloang yang tampak kekecilan
    Hasil flood-fill penghapusan latar putih sebelumnya menyisakan
    banyak ruang transparan di sekeliling lingkaran (isi cuma 74% tinggi
    kanvas), jadi walau tinggi CSS-nya sama dengan logo lain, lingkaran
    Kelurahan tampak lebih kecil. Kanvas di-crop pas ke tepi lingkaran
    (sekarang isi 100%) supaya proporsinya sama dengan logo lain.

--- docs/sw.js
+++ docs/sw.js
@@ -10,7 +10,7 @@
      lewat localStorage supaya ada label waktu pembaruan.
    ============================================================ */
 
-var VERSI_CACHE = 'gama-hijau-v35';
+var VERSI_CACHE = 'gama-hijau-v36';
 var CACHE_FONT = 'gama-font-v1';
 
 var ASET_APLIKASI = [
