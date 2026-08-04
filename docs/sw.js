1deffcbc8519f73052bf175a195b542efce0592c
gama-hijau <asnul05@gmail.com>
2026-08-05 03:09:23 +0800

    Kembalikan proporsi zoom Aisyah, Salsabila, Amru - jangan terlalu jauh
    Ronde sebelumnya kelewat longgar (wajah terlalu kecil, latar terlalu
    dominan). Ditarik lebih dekat lagi dengan margin kepala yang wajar
    (kecil, bukan nol dan bukan berlebihan) supaya proporsinya pas.

--- docs/sw.js
+++ docs/sw.js
@@ -10,7 +10,7 @@
      lewat localStorage supaya ada label waktu pembaruan.
    ============================================================ */
 
-var VERSI_CACHE = 'gama-hijau-v55';
+var VERSI_CACHE = 'gama-hijau-v56';
 var CACHE_FONT = 'gama-font-v1';
 
 var ASET_APLIKASI = [
